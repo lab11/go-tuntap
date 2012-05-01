e8e07c34575fce94f92909bd2f02ed03468f087c
David Anderson <dave@natulte.net>
2012-05-01 01:13:46 -0700

    Actually care about what type of interface the user wants.

--- tun_linux.go
+++ tun_linux.go
@@ -8,10 +8,10 @@ package tuntap
 #include <linux/if.h>
 #include <linux/if_tun.h>
 
-char *tun_ioctl(int fd, char *name) {
+char *tun_ioctl(int fd, int type, char *name) {
   struct ifreq req;
   memset(&req, 0, sizeof(req));
-  req.ifr_flags = IFF_TUN;
+  req.ifr_flags = type;
   strncpy(req.ifr_name, name, IFNAMSIZ);
   if (ioctl(fd, TUNSETIFF, (void*)&req)) {
     return NULL;
@@ -29,12 +29,21 @@ import (
 	"unsafe"
 )
 
-const FlagTruncated = C.TUN_PKT_STRIP
+const flagTruncated = C.TUN_PKT_STRIP
 
 func createInterface(file *os.File, ifPattern string, kind DevKind) (string, error) {
 	cIfPattern := C.CString(ifPattern)
 	defer C.free(unsafe.Pointer(cIfPattern))
-	cIfName, err := C.tun_ioctl(C.int(file.Fd()), cIfPattern)
+	var typ C.int
+	switch kind {
+	case DevTun:
+		typ = C.IFF_TUN
+	case DevTap:
+		typ = C.IFF_TAP
+	default:
+		panic("Unknown interface type")
+	}
+	cIfName, err := C.tun_ioctl(C.int(file.Fd()), typ, cIfPattern)
 	defer C.free(unsafe.Pointer(cIfName))
 	if cIfName == nil {
 		return "", err
